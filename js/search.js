3db8fd7e14e78597e46b8210f2a7e9e634086bdd
Lucas PenzeyMoog <lucaspenzeymoog@gmail.com>
2019-01-23 14:35:02 -0600

    refactored api search module and updated related tests

--- js/search.js
+++ js/search.js
@@ -2,12 +2,37 @@
 const axios = require("axios");
 const googleBooksUrl = "https://www.googleapis.com/books/v1/volumes?q=";
 const keys = require("../config/keys");
+const Book = require("./Book");
 
 const search = {
   fetchBooks: term =>
     axios
       .get(googleBooksUrl + term + "&key=" + keys.googleBooksApiKey)
-      .then(response => response.data.items)
+      .then(response => {
+        // Instantiate books array
+        const newLocal = [];
+        const books = newLocal;
+        // Loop through api response object
+        for (const item of response.data.items) {
+          // Set title
+          const title = item.volumeInfo.title;
+          // Set author
+          const authors = item.volumeInfo.authors;
+          // Set image link
+          const imageLink = item.volumeInfo.imageLinks.thumbnail;
+          // Set previeww link
+          const previewLink = item.volumeInfo.previewLink;
+          // Set isbn
+          const isbn = item.volumeInfo.industryIdentifiers[0].identifier;
+          // Create new Book with above parameters
+          const book = new Book(title, authors, imageLink, previewLink, isbn);
+          // Add to books array
+          books.push(book);
+        }
+        // Convert to JSON string
+        json_books = JSON.stringify(books);
+        return json_books;
+      })
       .catch(err => "error")
 };
 
