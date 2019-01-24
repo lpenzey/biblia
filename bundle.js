9909059f2cc6f1f8c642c495e0e5ec86f71f8c79
Lucas PenzeyMoog <lucaspenzeymoog@gmail.com>
2019-01-24 14:25:23 -0600

    rebundled bundle.js to reflect current build

--- bundle.js
+++ bundle.js
@@ -1,6 +1,4 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
-
-},{}],2:[function(require,module,exports){
 // shim for using process in browser
 var process = module.exports = {};
 
@@ -186,31 +184,36 @@ process.chdir = function (dir) {
 };
 process.umask = function() { return 0; };
 
-},{}],3:[function(require,module,exports){
+},{}],2:[function(require,module,exports){
 (function (process){
-if (process.env.NOD_ENV === "production") {
+if (process.env.NODE_ENV === "production") {
   module.exports = require("./keys_prod");
 } else {
   module.exports = require("./keys_dev");
 }
 
 }).call(this,require('_process'))
-},{"./keys_dev":1,"./keys_prod":4,"_process":2}],4:[function(require,module,exports){
+},{"./keys_dev":3,"./keys_prod":4,"_process":1}],3:[function(require,module,exports){
+module.exports = {
+  googleBooksApiKey: ""
+};
+
+},{}],4:[function(require,module,exports){
 (function (process){
 module.exports = {
   googleBooksApiKey: process.env.GOOGLE_BOOKS_API_KEY
 };
 
 }).call(this,require('_process'))
-},{"_process":2}],5:[function(require,module,exports){
-// Book Class: Represents a book
+},{"_process":1}],5:[function(require,module,exports){
+// Book Object: Represents a book
 
-class Book {
-  constructor(title, author, isbn) {
-    this.title = title;
-    this.author = author;
-    this.isbn = isbn;
-  }
+function Book(title, author, imageLink, previewLink, id) {
+  this.title = title;
+  this.author = author;
+  this.imageLink = imageLink;
+  this.previewLink = previewLink;
+  this.id = id;
 }
 
 module.exports = Book;
@@ -218,38 +221,66 @@ module.exports = Book;
 },{}],6:[function(require,module,exports){
 // search tasks
 const axios = require("axios");
-const googleBooksUrl = "https://www.googleapis.com/books/v1/volumes?q=";
+const googleBooksUrl = "https://www.googleapis.com/books/v1/volumes";
 const keys = require("../config/keys");
 const Book = require("./Book");
 
 const search = {
   fetchBooks: term =>
     axios
-      .get(googleBooksUrl + term + "&key=" + keys.googleBooksApiKey)
+      .get(googleBooksUrl + "?q=" + term + "&key=" + keys.googleBooksApiKey)
       .then(response => {
+        // Instantiate books array
         const newLocal = [];
         const books = newLocal;
-        for (i = 0; i < response.data.items.length; i++) {
-          const title = response.data.items[i].volumeInfo.title;
-          const authors = response.data.items[i].volumeInfo.authors;
-          const isbn = "123456789";
-          const book = new Book(title, authors, isbn);
+        // Loop through api response object
+        for (const item of response.data.items) {
+          // Set title
+          const title = item.volumeInfo.title;
+          // Set author
+          const authors = item.volumeInfo.authors;
+          // Set image link
+          const imageLink = item.volumeInfo.imageLinks.thumbnail;
+          // Set previeww link
+          const previewLink = item.volumeInfo.previewLink;
+          // Set isbn
+          const id = item.id;
+          // Create new Book with above parameters
+          const book = new Book(title, authors, imageLink, previewLink, id);
+          // Add to books array
           books.push(book);
         }
-        return books;
+        // Convert to JSON string
+        json_books = JSON.stringify(books);
+        return json_books;
       })
       .catch(err => "error")
 };
-
 module.exports = search;
 
-},{"../config/keys":3,"./Book":5,"axios":9}],7:[function(require,module,exports){
+},{"../config/keys":2,"./Book":5,"axios":9}],7:[function(require,module,exports){
 const UI = {
-  displayBooks() {
+  displayBook(book) {
+    // Grab handlebars template in index.html
+    const source = document.querySelector("#book-template").innerHTML;
+
+    // Create handlebars template
+    const template = Handlebars.compile(source);
+
+    // Create new book card
+    const newHtml = template(book);
+
+    // Append each book to the list
+    document.querySelector("#book-list").innerHTML += newHtml;
+  },
+  displayStoredBooks() {
     console.log("Here are the books");
   },
-  clearFields() {
-    document.querySelector("#title").value = "";
+  clearField(element) {
+    document.querySelector(element).value = "";
+  },
+  clearSearchedBooks(element) {
+    document.querySelector(element).value = "";
   }
 };
 
@@ -262,7 +293,7 @@ const search = require("./js/search");
 
 // Event: Display Books
 document.addEventListener("DOMContentLoaded", UI.displayBooks);
-// Event: Add a book
+// Event: Search for books
 document.querySelector("#book-form").addEventListener("submit", e => {
   // Get Form Values
   // Prevent default
@@ -274,19 +305,27 @@ document.querySelector("#book-form").addEventListener("submit", e => {
     console.log("Please fill in all fields", "danger");
   } else {
     // Perform google books search
-    gbooks = search.fetchBooks(title);
-    console.log(gbooks);
+    search.fetchBooks(title).then(result => {
+      json_books = JSON.parse(result);
+      for (const item of json_books) {
+        UI.displayBook(item);
+      }
+    });
+    //const booksArray = JSON.parse(gbooks);
+
     // Instantiate book
 
-    // Add book to UI
     // Add book to store
+
     // Store.addBook(book);
 
     // Show success message
+
     // UI.showAlert("Book Added", "success");
 
     // Clear Fields
-    UI.clearFields();
+    UI.clearField("#title");
+    UI.clearSearchedBooks("#book-list");
   }
 });
 
@@ -476,7 +515,7 @@ module.exports = function xhrAdapter(config) {
 };
 
 }).call(this,require('_process'))
-},{"../core/createError":17,"./../core/settle":20,"./../helpers/btoa":24,"./../helpers/buildURL":25,"./../helpers/cookies":27,"./../helpers/isURLSameOrigin":29,"./../helpers/parseHeaders":31,"./../utils":33,"_process":2}],11:[function(require,module,exports){
+},{"../core/createError":17,"./../core/settle":20,"./../helpers/btoa":24,"./../helpers/buildURL":25,"./../helpers/cookies":27,"./../helpers/isURLSameOrigin":29,"./../helpers/parseHeaders":31,"./../utils":33,"_process":1}],11:[function(require,module,exports){
 'use strict';
 
 var utils = require('./utils');
@@ -1033,7 +1072,7 @@ utils.forEach(['post', 'put', 'patch'], function forEachMethodWithData(method) {
 module.exports = defaults;
 
 }).call(this,require('_process'))
-},{"./adapters/http":10,"./adapters/xhr":10,"./helpers/normalizeHeaderName":30,"./utils":33,"_process":2}],23:[function(require,module,exports){
+},{"./adapters/http":10,"./adapters/xhr":10,"./helpers/normalizeHeaderName":30,"./utils":33,"_process":1}],23:[function(require,module,exports){
 'use strict';
 
 module.exports = function bind(fn, thisArg) {
