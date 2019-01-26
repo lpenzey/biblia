6bf849fb4ac07a7dd5c092291b1d0a035c99ee3a
Lucas PenzeyMoog <lucaspenzeymoog@gmail.com>
2019-01-26 13:02:48 -0600

    added logic for retrieving and displaying next 10 items

--- main.js
+++ main.js
@@ -1,20 +1,24 @@
 const UI = require("./js/ui");
 const Book = require("./js/Book");
 const search = require("./js/search");
+const store = require("./js/store");
 
-// Event: Display Books
-document.addEventListener("DOMContentLoaded", UI.displayBooks);
+// Event: Display Stored Books
+document.addEventListener("DOMContentLoaded", UI.displayStoredBooks);
 // Event: Search for books
 document.querySelector("#book-form").addEventListener("submit", e => {
-  // Get Form Values
   // Prevent default
   e.preventDefault();
+  // Get Form Values
   const title = document.querySelector("#title").value;
 
-  // Validate
+  // Validate input
   if (title === "") {
     console.log("Please fill in all fields", "danger");
   } else {
+    // Store query and index in session storage
+    store.setLatestQuery(title);
+    store.setIndex(0);
     // Perform google books search
     search.fetchBooks(title).then(result => {
       json_books = JSON.parse(result);
@@ -22,20 +26,31 @@ document.querySelector("#book-form").addEventListener("submit", e => {
         UI.displayBook(item);
       }
     });
-    //const booksArray = JSON.parse(gbooks);
-
-    // Instantiate book
-
-    // Add book to store
-
-    // Store.addBook(book);
 
     // Show success message
 
-    // UI.showAlert("Book Added", "success");
-
-    // Clear Fields
+    // Clear search field
     UI.clearField("#title");
-    UI.clearSearchedBooks("#book-list");
   }
+});
+//Event: Search for 10 more books
+document.querySelector("#book-load").addEventListener("click", e => {
+  // Retrieve index from session storage and convert to integer w/ base 10
+  var index = parseInt(sessionStorage.index, 10);
+  // Increment search index by 10
+  index = index += 10;
+  console.log(index);
+  // Update index in session storage
+  store.setIndex(index);
+
+  e.preventDefault;
+  // Search for more books using the current query and updated index
+  search.fetchBooks(sessionStorage.latestQuery, index).then(result => {
+    console.log(result);
+    json_books = JSON.parse(result);
+    // Iterate through parsed API response and add to UI
+    for (const item of json_books) {
+      UI.displayBook(item);
+    }
+  });
 });
