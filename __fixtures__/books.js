2b28b1443bf6b035390134f36e0669cbd911f158
Lucas PenzeyMoog <lucaspenzeymoog@gmail.com>
2019-01-25 18:36:54 -0600

    added edge cases for books with missing fields

--- __fixtures__/books.js
+++ __fixtures__/books.js
@@ -18,5 +18,43 @@ module.exports = {
     previewLink:
       "http://books.google.com/books?id=zWzAAgAAQBAJ&printsec=frontcover&dq=tolkein&hl=&cd=2&source=gbs_api",
     id: "zWzAAgAAQBAJ"
+  },
+  missingPublishingCompany: {
+    title: "J.R.R. Tolkien",
+    author: ["Humphrey Carpenter"],
+    publishingCompany: "",
+    imageLink:
+      "http://books.google.com/books/content?id=zWzAAgAAQ…=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+    previewLink:
+      "http://books.google.com/books?id=zWzAAgAAQBAJ&printsec=frontcover&dq=tolkein&hl=&cd=2&source=gbs_api",
+    id: "zWzAAgAAQBAJ"
+  },
+  missingImageLink: {
+    title: "J.R.R. Tolkien",
+    author: ["Humphrey Carpenter"],
+    publishingCompany: "Houghton Mifflin Harcourt",
+    imageLink: "",
+    previewLink:
+      "http://books.google.com/books?id=zWzAAgAAQBAJ&printsec=frontcover&dq=tolkein&hl=&cd=2&source=gbs_api",
+    id: "zWzAAgAAQBAJ"
+  },
+  missingPreviewLink: {
+    title: "J.R.R. Tolkien",
+    author: ["Humphrey Carpenter"],
+    publishingCompany: "Houghton Mifflin Harcourt",
+    imageLink:
+      "http://books.google.com/books/content?id=zWzAAgAAQ…=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+    previewLink: "",
+    id: "zWzAAgAAQBAJ"
+  },
+  missingId: {
+    title: "J.R.R. Tolkien",
+    author: ["Humphrey Carpenter"],
+    publishingCompany: "Houghton Mifflin Harcourt",
+    imageLink:
+      "http://books.google.com/books/content?id=zWzAAgAAQ…=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+    previewLink:
+      "http://books.google.com/books?id=zWzAAgAAQBAJ&printsec=frontcover&dq=tolkein&hl=&cd=2&source=gbs_api",
+    id: ""
   }
 };
