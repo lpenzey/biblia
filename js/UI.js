392e68458e43177b21579b85755a691cd3eeb2b8
Lucas PenzeyMoog <lucaspenzeymoog@gmail.com>
2019-01-27 10:58:42 -0600

    added handlebars templating function to display book to page

--- js/UI.js
+++ js/UI.js
@@ -1,11 +1,38 @@
+const Handlebars = require("handlebars");
+
 const UI = {
-  displayBooks() {
-    console.log("Here are the books");
+  displayBook(book) {
+    // Get  handlebars template in index.html
+
+    const source = document.querySelector("#book-template").innerHTML;
+
+    // Create handlebars template
+    const template = Handlebars.compile(source);
+
+    // Create new book card
+    const newHtml = template(book);
+
+    // Append each book to the list
+    document.querySelector("#book-list").innerHTML += newHtml;
   },
-  clearFields() {
-    document.querySelector("#title").value = "";
-    document.querySelector("#author").value = "";
-    document.querySelector("#isbn").value = "";
+  enlargeImage(book, size) {
+    str = book.imageLink;
+    const newStr = str.replace("&zoom=1", "&zoom=" + size);
+    book.imageLink = newStr;
+  },
+  clearField(element) {
+    document.querySelector(element).value = "";
+  },
+  clearSearchedBooks(element) {
+    document.querySelector(element).innerHTML = null;
+  },
+  showButtons() {
+    document.querySelector("#book-load").style.visibility = "visible";
+    document.querySelector("#book-clear").style.visibility = "visible";
+  },
+  hideButtons() {
+    document.querySelector("#book-load").style.visibility = "hidden";
+    document.querySelector("#book-clear").style.visibility = "hidden";
   }
 };
 
