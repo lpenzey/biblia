94ddf76fb608b813a3253f22308c38f3f6413c3a
Lucas PenzeyMoog <lucaspenzeymoog@gmail.com>
2019-01-27 11:02:21 -0600

    added event listener for button to clear searched books

--- main.js
+++ main.js
@@ -3,8 +3,8 @@ const Book = require("./js/Book");
 const search = require("./js/search");
 const store = require("./js/store");
 
-// Event: Display Stored Books
-document.addEventListener("DOMContentLoaded", UI.displayStoredBooks);
+// Event: Hide Buttons
+document.addEventListener("DOMContentLoaded", UI.hideButtons);
 // Event: Search for books
 document.querySelector("#book-form").addEventListener("submit", e => {
   // Prevent default
@@ -19,12 +19,14 @@ document.querySelector("#book-form").addEventListener("submit", e => {
     // Store query and index in session storage
     store.setLatestQuery(title);
     store.setIndex(0);
+
     // Perform google books search
     search.fetchBooks(title).then(result => {
       json_books = JSON.parse(result);
       for (const item of json_books) {
         UI.displayBook(item);
       }
+      UI.showButtons();
     });
 
     // Show success message
@@ -39,18 +41,24 @@ document.querySelector("#book-load").addEventListener("click", e => {
   var index = parseInt(sessionStorage.index, 10);
   // Increment search index by 10
   index = index += 10;
-  console.log(index);
   // Update index in session storage
   store.setIndex(index);
 
   e.preventDefault;
+  // Retrieve current search query
+  const title = sessionStorage.latestQuery;
   // Search for more books using the current query and updated index
-  search.fetchBooks(sessionStorage.latestQuery, index).then(result => {
-    console.log(result);
-    json_books = JSON.parse(result);
+  search.fetchBooks(title, index).then(result => {
+    books = JSON.parse(result);
     // Iterate through parsed API response and add to UI
-    for (const item of json_books) {
+    for (const item of books) {
       UI.displayBook(item);
     }
   });
+});
+
+// Event: clear searched books
+document.querySelector("#book-clear").addEventListener("click", e => {
+  UI.clearSearchedBooks("#book-list");
+  UI.hideButtons();
 });
