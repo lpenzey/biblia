c260a70c099b3302c2fe9704d797896d0e129528
Lucas PenzeyMoog <lucaspenzeymoog@gmail.com>
2019-01-27 19:37:36 -0600

    changed publishingCompany to publisher

--- js/book.js
+++ js/book.js
@@ -1,9 +1,9 @@
 // Book Object: Represents a book
 
-function Book(title, author, publishingCompany, imageLink, previewLink, id) {
+function Book(title, author, publisher, imageLink, previewLink, id) {
   this.title = title;
   this.author = author;
-  this.publishingCompany = publishingCompany;
+  this.publisher = publisher;
   this.imageLink = imageLink;
   this.previewLink = previewLink;
   this.id = id;
