e2572655b484bdc6bd19f62cbc8da8aa7aa994f2
Lucas PenzeyMoog <lucaspenzeymoog@gmail.com>
2019-01-27 18:08:25 -0600

    added tests for parseBooks function

--- __tests__/search.test.js
+++ __tests__/search.test.js
@@ -3,21 +3,55 @@ const search = require("../js/search");
 const keys = require("../config/keys");
 const searchResponse = require("../__fixtures__/searchResponse");
 
-test("fetches results from google books api and formats correctly", () => {
-  // Setup axios mock to respond with typical google books api response
+test("fetches results from google books api", () => {
   mockAxios.get.mockImplementationOnce(() =>
     Promise.resolve(searchResponse.raw)
   );
 
-  // execute search function with parameter "tolkein"
+  let term = "tolkein";
+  let index = 0;
+
   return search.fetchBooks("tolkein").then(response => {
-    // Response from fetchBooks should be a stringified array of book objects
-    expect(response).toEqual(searchResponse.stringified);
+    expect(response).toEqual(searchResponse.raw);
     expect(mockAxios.get).toHaveBeenCalledTimes(1);
     expect(mockAxios.get).toHaveBeenCalledWith(
-      "https://www.googleapis.com/books/v1/volumes?q=tolkein&startIndex=0&key=" +
-        keys.googleBooksApiKey
+      "https://www.googleapis.com/books/v1/volumes",
+      {
+        params: {
+          q: term,
+          startIndex: index,
+          key: keys.googleBooksApiKey
+        }
+      }
     );
   });
 });
-test("increases the index by 10 when more books button is clicked", () => {});
+
+test("fetches results from google books api with start index of 10", () => {
+  mockAxios.get.mockImplementationOnce(() =>
+    Promise.resolve(searchResponse.raw)
+  );
+
+  let term = "tolkein";
+  let index = 10;
+
+  return search.fetchBooks("tolkein", 10).then(response => {
+    expect(response).toEqual(searchResponse.raw);
+    expect(mockAxios.get).toHaveBeenCalledTimes(2);
+    expect(mockAxios.get).toHaveBeenCalledWith(
+      "https://www.googleapis.com/books/v1/volumes",
+      {
+        params: {
+          q: term,
+          startIndex: index,
+          key: keys.googleBooksApiKey
+        }
+      }
+    );
+  });
+});
+
+test("parseBooks correctly parses full response object", () => {
+  booksArr = search.parseBooks(searchResponse.raw);
+  expect(booksArr).toEqual(searchResponse.parsed);
+});
