c66fe2023e6a43b8a8a8dc10aa40ca688a79649b
Lucas PenzeyMoog <lucaspenzeymoog@gmail.com>
2019-01-27 19:41:09 -0600

    extracted anonymous event handler functions in main.js to eventHandlers module

---
+++ js/eventHandlers.js
@@ -0,0 +1,37 @@
+const UI = require("./ui");
+const search = require("./search");
+const store = require("./store");
+
+const eventHandlers = {
+  bookForm() {
+    const title = document.querySelector("#title").value;
+    if (title === "") {
+      console.log("Please fill in all fields", "danger");
+    } else {
+      store.setLatestQuery(title);
+      store.setIndex(0);
+      search.fetchBooks(title).then(response => {
+        booksArr = search.parseBooks(response);
+        for (const item of booksArr) {
+          UI.displayBook(item);
+        }
+        UI.showButtons();
+      });
+      UI.clearField("#title");
+    }
+  },
+  loadMoreBooks() {
+    var index = parseInt(sessionStorage.index, 10);
+    index = index += 10;
+    store.setIndex(index);
+    const title = sessionStorage.latestQuery;
+    search.fetchBooks(title, index).then(response => {
+      booksArr = search.parseBooks(response);
+      for (const item of booksArr) {
+        UI.displayBook(item);
+      }
+    });
+  }
+};
+
+module.exports = eventHandlers;
--- main.js
+++ main.js
@@ -1,60 +1,18 @@
 const UI = require("./js/ui");
-const search = require("./js/search");
-const store = require("./js/store");
+const eventHandlers = require("./js/eventHandlers");
 
-document.addEventListener("DOMContentLoaded", UI.hideButtons);
+document.addEventListener("DOMContentLoaded", UI.hideButtons());
 
 document.querySelector("#book-form").addEventListener("submit", e => {
   e.preventDefault();
-  const title = document.querySelector("#title").value;
-
-  // Validate input
-  if (title === "") {
-    console.log("Please fill in all fields", "danger");
-  } else {
-    // Store query and index in session storage
-    store.setLatestQuery(title);
-    store.setIndex(0);
-
-    search.fetchBooks(title).then(response => {
-      booksArr = search.parseBooks(response);
-
-      for (const item of booksArr) {
-        UI.displayBook(item);
-      }
-
-      UI.showButtons();
-    });
-
-    // Show success message
-
-    // Clear search field
-    UI.clearField("#title");
-  }
+  eventHandlers.bookForm();
 });
-//Event: Search for 10 more books
+
 document.querySelector("#book-load").addEventListener("click", e => {
-  // Retrieve index from session storage and convert to integer w/ base 10
-  var index = parseInt(sessionStorage.index, 10);
-  // Increment search index by 10
-  index = index += 10;
-  // Update index in session storage
-  store.setIndex(index);
-
-  e.preventDefault;
-  // Retrieve current search query
-  const title = sessionStorage.latestQuery;
-  // Search for more books using the current query and updated index
-  search.fetchBooks(title).then(response => {
-    booksArr = search.parseBooks(response);
-
-    for (const item of booksArr) {
-      UI.displayBook(item);
-    }
-  });
+  e.preventDefault();
+  eventHandlers.loadMoreBooks();
 });
 
-// Event: clear searched books
 document.querySelector("#book-clear").addEventListener("click", e => {
   UI.clearSearchedBooks("#book-list");
   UI.hideButtons();
