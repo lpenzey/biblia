0017e89e1deb2c8005b5f8a92eefdf158fdc5500
Lucas PenzeyMoog <lucaspenzeymoog@gmail.com>
2019-02-05 14:13:22 -0600

    updated test title

--- __tests__/ui.test.js
+++ __tests__/ui.test.js
@@ -3,7 +3,7 @@ const bookObject = require("../__fixtures__/books");
 const handlebars = require("../__fixtures__/handlebars");
 const html = require("../__fixtures__/html");
 
-test("displayBook gets Handlebars template from document", () => {
+test("displayBook gets Handlebars template from document and displays correctly", () => {
   document.body.innerHTML = html.empty;
 
   const book = bookObject.complete;
